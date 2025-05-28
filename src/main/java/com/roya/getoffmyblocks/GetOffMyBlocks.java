f000bd14de9716bf23313dd990a86d57704841da
Roya <roya.bunbun@gmail.com>
2025-05-28 19:55:08 -0400

    Update GetOffMyBlocks.java
    added what Claude thinks is necessary for the main file lmao

--- src/main/java/com/roya/getoffmyblocks/GetOffMyBlocks.java
+++ src/main/java/com/roya/getoffmyblocks/GetOffMyBlocks.java
@@ -1,1 +1,32 @@
 // MAIN MOD FILE
+
+package com.roya.getoffmyblocks;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GetOffMyBlocks implements ModInitializer {
+    public static final String MOD_ID = "getoffmyblocks";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("getoffmyblocks initializing...");
+        
+        // Initialize the block manager
+        BlockSpawnManager.initialize();
+        
+        // Register commands
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            GombCommand.register(dispatcher);
+        });
+        
+        // Register Cobblemon spawn event listener
+        CobblemonSpawnHandler.register();
+        
+        LOGGER.info("getoffmyblocks initialized");
+    }
+}
